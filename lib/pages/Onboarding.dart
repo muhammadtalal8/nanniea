c47f697a0ea7f3feecd6563847ee5d0bbfd0bbf4
unknown <muhammadtalalshakeel2009@gmail.com>
2025-03-19 01:30:07 +0900

    first commit

--- lib/pages/Onboarding.dart
+++ lib/pages/Onboarding.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: camel_case_types, library_private_types_in_public_api, use_key_in_widget_constructors, file_names
+// ignore_for_file: camel_case_types, library_private_types_in_public_api, use_key_in_widget_constructors, file_names, unused_field
 
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends StatefulWidget {
   @override
@@ -14,138 +13,374 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    return Container(
+      width: 375,
+      height: 812,
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(color: Colors.white),
+      child: Stack(
         children: [
-          PageView(
-            controller: _controller,
-            onPageChanged: (index) {
-              setState(() {
-                isLastPage = index == 2; // Change if you add more pages
-              });
-            },
-            children: [
-              OnboardingPage(
-                image: "assets/kid1.png",
-                title: "Welcome to MyApp",
-                description: "Discover amazing features with our app.",
+          Positioned(
+            left: 0,
+            top: 778,
+            child: Container(
+              width: 375,
+              height: 34,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Container(width: 375, height: 34),
+                  ),
+                  Positioned(
+                    left: 121,
+                    top: 20,
+                    child: Container(
+                      width: 134,
+                      height: 5,
+                      decoration: ShapeDecoration(
+                        color: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              OnboardingPage(
-                image: "assets/kid2.png",
-                title: "Stay Connected",
-                description: "Always stay in touch with your favorite content.",
-              ),
-              OnboardingPage(
-                image: "assets/kid3.png",
-                title: "Get Started",
-                description: "Enjoy a seamless experience.",
-              ),
-            ],
+            ),
           ),
           Positioned(
-            bottom: 80,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: SmoothPageIndicator(
-                controller: _controller,
-                count: 3,
-                effect: SlideEffect(
-                  activeDotColor: Color.fromRGBO(104, 170, 255, 1),
-                  dotHeight: 10,
-                  dotWidth: 10,
-                  dotColor: Colors.grey,
+            left: 170,
+            top: 732,
+            child: Container(
+              width: 35,
+              height: 22,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Text(
+                      'Skip',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF666666),
+                        fontSize: 17,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 1.29,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 169,
+            top: 685,
+            child: Container(
+              width: 7,
+              height: 7,
+              decoration: ShapeDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.00, 0.00),
+                  end: Alignment(1.00, 1.00),
+                  colors: [Color(0xFF68A9FF), Color(0xFFFF96C9)],
+                ),
+                shape: OvalBorder(
+                  side: BorderSide(
+                    width: 1,
+                    strokeAlign: BorderSide.strokeAlignOutside,
+                  ),
                 ),
               ),
             ),
           ),
           Positioned(
-            bottom: 20,
-            right: 0,
-            left: 0,
-            child:
-                isLastPage
-                    ? ElevatedButton(
-                      onPressed: () {
-                        // Navigate to Home Screen
-                        Navigator.pushReplacementNamed(context, '/home');
-                      },
-                      child: Text("Get Started"),
-                    )
-                    : Center(
-                      child: TextButton(
-                        onPressed: () {
-                          _controller.nextPage(
-                            duration: Duration(milliseconds: 500),
-                            curve: Curves.easeInOut,
-                          );
-                        },
-                        child: Text("Skip"),
+            left: 184,
+            top: 685,
+            child: Opacity(
+              opacity: 0.40,
+              child: Container(
+                width: 7,
+                height: 7,
+                decoration: ShapeDecoration(
+                  color: Color(0xFF666666),
+                  shape: OvalBorder(
+                    side: BorderSide(
+                      width: 1,
+                      strokeAlign: BorderSide.strokeAlignOutside,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 199,
+            top: 685,
+            child: Opacity(
+              opacity: 0.40,
+              child: Container(
+                width: 7,
+                height: 7,
+                decoration: ShapeDecoration(
+                  color: Color(0xFF666666),
+                  shape: OvalBorder(
+                    side: BorderSide(
+                      width: 1,
+                      strokeAlign: BorderSide.strokeAlignOutside,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 40,
+            top: 495,
+            child: Container(
+              width: 295,
+              height: 150,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 84,
+                    child: SizedBox(
+                      width: 295,
+                      child: Text(
+                        'Find the perfect nannies in your own neighborhood for your beloved child quickly and easily',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFF666666),
+                          fontSize: 17,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 1.29,
+                        ),
                       ),
                     ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 28,
+                    child: SizedBox(
+                      width: 295,
+                      child: Text(
+                        'Discover',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 106,
+                    top: 0,
+                    child: Text(
+                      'Geolocation',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF68A9FF),
+                        fontSize: 15,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 1.33,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 0,
+            child: Container(
+              width: 375,
+              height: 427,
+              clipBehavior: Clip.antiAlias,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Container(
+                      width: 375,
+                      height: 427,
+                      decoration: BoxDecoration(color: Color(0xFFC8C7CC)),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Container(
+                      width: 375,
+                      height: 427,
+                      decoration: BoxDecoration(color: Color(0xFFC8C7CC)),
+                    ),
+                  ),
+                  Positioned(
+                    left: 70,
+                    top: 185,
+                    child: Text(
+                      '375 x 427',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF8A8A8F),
+                        fontSize: 48,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Opacity(
+                      opacity: 0.20,
+                      child: Container(
+                        width: 375,
+                        height: 427,
+                        decoration: BoxDecoration(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 5,
+                    top: -64,
+                    child: Container(
+                      width: 370,
+                      height: 555,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage("https://placehold.co/370x555"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 160,
+            top: 399,
+            child: Container(
+              width: 56,
+              height: 56,
+              decoration: ShapeDecoration(
+                color: Color(0xFF4CD964),
+                shape: OvalBorder(),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 0,
+            child: Container(
+              width: 375,
+              height: 44,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Container(width: 375, height: 44),
+                  ),
+                  Positioned(
+                    left: 336,
+                    top: 18.33,
+                    child: Opacity(
+                      opacity: 0.35,
+                      child: Container(
+                        width: 22,
+                        height: 11.33,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1, color: Colors.white),
+                            borderRadius: BorderRadius.circular(2.67),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 338,
+                    top: 20.33,
+                    child: Container(
+                      width: 18,
+                      height: 7.33,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(1.33),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 21,
+                    top: 16,
+                    child: Container(
+                      width: 54,
+                      height: 16,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: SizedBox(
+                              width: 54,
+                              child: Text(
+                                '9:12',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
-    );
-  }
-}
-
-class OnboardingPage extends StatelessWidget {
-  final String image;
-  final String title;
-  final String description;
-
-  const OnboardingPage({
-    required this.image,
-    required this.title,
-    required this.description,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Expanded(
-          flex: 3, // Image section takes more space
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blue.shade50,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-              image: DecorationImage(
-                image: AssetImage(image),
-                fit: BoxFit.cover, // Ensures image fills the box
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 2, // Text content takes less space
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  description,
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
