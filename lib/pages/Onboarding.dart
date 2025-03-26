f91d250ed7b9683f940b80e01db9f04adfd922f3
unknown <muhammadtalalshakeel2009@gmail.com>
2025-03-26 23:50:12 +0330

    first commit

--- lib/pages/Onboarding.dart
+++ lib/pages/Onboarding.dart
@@ -10,25 +10,62 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final List<Widget> _onboardingScreens = [
-    OnboardingCard(image: "assets/kid1.png"),
+    OnboardingCard(
+      image: "assets/kid1.png",
+      subtitle: "Geolocation",
+      title: "Discover",
+      description:
+          "Find the perfect nannies in your own neighborhood for your beloved child quickly and easily",
+    ),
+    OnboardingCard(
+      image: "assets/kid2.png",
+      subtitle: "Availability",
+      title: "Schedule",
+      description:
+          "Determine the right time between you and the nannies with choices you cannot imagine before",
+    ),
+    OnboardingCard(
+      image: "assets/kid3.png",
+      subtitle: "Always",
+      title: "Connected",
+      description:
+          "Stay update your child's activity wherever and whenever in real time with messages or video calls",
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white, // Set the background color to black
+        color: Colors.white,
         child: Column(
           children: [
             Expanded(child: PageView(children: _onboardingScreens)),
             Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 180,
-                vertical: 120,
+                vertical: 80,
               ),
               child: Row(
                 mainAxisSize: MainAxisSize.max,
                 children: [for (int i = 0; i < 3; i++) _buildDot(i == 0)],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Skip",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: const Color(0xFF666666),
+                    fontSize: 17,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    height: 1.29,
+                  ),
+                ),
               ),
             ),
           ],
--- lib/widgets/onboarding_card.dart
+++ lib/widgets/onboarding_card.dart
@@ -1,65 +1,76 @@
 import 'package:flutter/material.dart';
 
 class OnboardingCard extends StatelessWidget {
-  final String image;
-  const OnboardingCard({super.key, required this.image});
+  final String image, title, description, subtitle;
+  const OnboardingCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.description,
+    required this.subtitle,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 427,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(50),
-              bottomRight: Radius.circular(50),
+    return Expanded(
+      child: Column(
+        children: [
+          Container(
+            height: 427,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 128),
-        Text(
-          'Geolocation',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: const Color(0xFF68A9FF),
-            fontSize: 15,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w400,
-            height: 1.33,
-          ),
-        ),
-
-        SizedBox(
-          width: 295,
-          child: Text(
-            'Discover',
+          SizedBox(height: 95),
+          Text(
+            subtitle,
             textAlign: TextAlign.center,
             style: TextStyle(
-              color: Colors.black,
-              fontSize: 40,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 295,
-          child: Text(
-            'Find the perfect nannies in your own neighborhood for your beloved child quickly and easily',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: const Color(0xFF666666),
-              fontSize: 17,
+              color: const Color(0xFF68A9FF),
+              fontSize: 15,
               fontFamily: 'Inter',
               fontWeight: FontWeight.w400,
-              height: 1.29,
+              height: 1.33,
             ),
           ),
-        ),
-      ],
+
+          SizedBox(
+            width: 295,
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 295,
+            child: Text(
+              description,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: const Color(0xFF666666),
+                fontSize: 17,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w400,
+                height: 1.29,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
